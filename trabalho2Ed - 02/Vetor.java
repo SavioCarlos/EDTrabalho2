47b048ceea16092504f27f7f8af8276edb95dbb2
SavioCarlos <110927106+SavioCarlos@users.noreply.github.com>
2024-05-22 23:51:28 -0300

    Update Vetor.java

--- trabalho2Ed - 02/Vetor.java
+++ trabalho2Ed - 02/Vetor.java
@@ -4,45 +4,41 @@ public class Vetor {
     private Integer[] dados;
     private int tamanho;
     private int capacidade;
-    
+
     public Vetor(int capacidade) {
         this.capacidade = capacidade;
         this.dados = new Integer[capacidade];
         this.tamanho = 0;
     }
-    
+
     public Vetor(Integer[] dados) {
         this.capacidade = dados.length;
         this.dados = Arrays.copyOf(dados, dados.length);
         this.tamanho = dados.length;
     }
-    
+
     public void inserir(int valor) {
         if (tamanho < capacidade) {
             dados[tamanho++] = valor;
         }
     }
-    
+
     public void bubbleSort() {
         for (int i = 0; i < tamanho - 1; i++) {
             for (int j = 0; j < tamanho - 1 - i; j++) {
                 if (dados[j] > dados[j + 1]) {
-                
-                
                     int temp = dados[j];
-                    
                     dados[j] = dados[j + 1];
                     dados[j + 1] = temp;
-                    
                 }
             }
         }
     }
-    
+
     public void mergeSort() {
         mergeSort(0, tamanho - 1);
     }
-    
+
     private void mergeSort(int esquerda, int direita) {
         if (esquerda < direita) {
             int meio = (esquerda + direita) / 2;
@@ -51,24 +47,24 @@ public class Vetor {
             merge(esquerda, meio, direita);
         }
     }
-    
+
     private void merge(int esquerda, int meio, int direita) {
         int n1 = meio - esquerda + 1;
         int n2 = direita - meio;
-        
+
         int[] L = new int[n1];
         int[] R = new int[n2];
-        
+
         for (int i = 0; i < n1; i++) {
             L[i] = dados[esquerda + i];
         }
         for (int j = 0; j < n2; j++) {
             R[j] = dados[meio + 1 + j];
         }
-        
+
         int i = 0, j = 0;
         int k = esquerda;
-        
+
         while (i < n1 && j < n2) {
             if (L[i] <= R[j]) {
                 dados[k++] = L[i++];
@@ -76,16 +72,16 @@ public class Vetor {
                 dados[k++] = R[j++];
             }
         }
-        
+
         while (i < n1) {
             dados[k++] = L[i++];
         }
-        
+
         while (j < n2) {
             dados[k++] = R[j++];
         }
     }
-    
+
     public int buscaSequencial(int valor) {
         for (int i = 0; i < tamanho; i++) {
             if (dados[i] == valor) {
@@ -94,7 +90,7 @@ public class Vetor {
         }
         return -1;
     }
-    
+
     public int buscaBinaria(int valor) {
         int esquerda = 0, direita = tamanho - 1;
         while (esquerda <= direita) {
